81533934742464f2e9c680afb03dd65c8ddc82f0
sethlarkin <sethlarkin@gmail.com>
2020-04-17 14:48:20 -0700

    adds server.go

---
+++ server.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+		var news = "FDLKNLKN"
+		fmt.Fprintf(w, news)
+
+	})
+
+	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hi")
+	})
+
+	log.Fatal(http.ListenAndServe(":8081", nil))
+
+}
+
+func newStr() {
+	var new = "NEW STR"
+	return new
+}
