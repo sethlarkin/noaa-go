09039ecd3fd0d03ab3e441b82de5f471b656fdd6
sethlarkin <sethlarkin@gmail.com>
2020-04-18 07:36:14 -0700

    Removes server.go for now

--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"json"
 	"log"
 	"net/http"
 )
@@ -12,7 +10,9 @@ func main() {
 	resp, _ := http.Get("https://api.weather.gov/gridpoints/TOP/31,80/forecast")
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "resp, %q", json.Decoder(resp))
+		// fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+		fmt.Fprintf(w, "resp, %q", resp)
+		// fmt.Println(resp, err)
 	})
 
 	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
